16b9f40c959330bdf5236f9c5a7077c307c8a9b3
hukahuka77 <andrew.kettenhofen@gmail.com>
2025-11-01 20:57:34 -0700

    as string again

--- frontend/src/app/edit/[id]/page.tsx
+++ frontend/src/app/edit/[id]/page.tsx
@@ -23,7 +23,7 @@ import AddItemChoiceModal from "@/components/places/AddItemChoiceModal";
 import PlacePickerModal from "@/components/places/PlacePickerModal";
 import { LIMITS } from "@/constants/limits";
 
-const API_BASE = process.env.NEXT_PUBLIC_API_BASE_URL;
+const API_BASE = process.env.NEXT_PUBLIC_API_BASE_URL as string;
 const BUCKET_NAME = process.env.NEXT_PUBLIC_SUPABASE_FOOD_ACTIVITIES_BUCKET as string;
 
 type GuidebookDetail = {
